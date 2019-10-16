17d62ec9a2ee20a27ae87727f41c6ed450d66102
yahir <yahirisaacurbina@gmail.com>
2019-10-15 18:39:29 -0600

    cuarto commit
    post/get

--- routes/api/fotografias/index.js
+++ routes/api/fotografias/index.js
@@ -20,4 +20,43 @@ router.get('/', function (req, res) {
 
  
 
+  
+router.post('/new', function(req, res){
+    foCollection = fileModel.getfotografias();
+    var newfotografias = Object.assign(
+       {},
+       req.body,
+       {
+           "title": req.body.nombre,
+           "url":req.body.apartament,
+           "thumbnailUrl":req.body.thum,
+           "album ":req.body.album
+           //"cuotaMensual": parseFloat(req.body.cuotaMensual)
+       }
+    );
+    var fotografiasExists = foCollection.find(
+      function(o, i){
+        return o.codigo === newfotografias.codigo;
+      }
+    )
+    if( ! fotografiasExists ){
+      foCollection.push(newfotografias);
+      fileModel.setfotografias(
+         foCollection,
+         function(err, savedSuccesfully){
+           if(err){
+             res.status(400).json({ "error": "No se pudo ingresar objeto" });
+           } else {
+             res.json(newfotografias);  // req.body ===  $_POST[]
+           }
+         }
+       );
+    } else {
+      res.status(400).json({"error":"No se pudo ingresar objeto"});
+    }
+ });
+
+
+
+
   module.exports = router;
